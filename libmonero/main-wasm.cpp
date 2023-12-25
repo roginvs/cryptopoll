27f4d23858af165f5124af0327d3478f6d67dbbb
Vasilii Rogin <vasya@rogin.ru>
2023-12-25 18:30:04 +0200

    Some wasm build

---
+++ libmonero/main-wasm.cpp
@@ -0,0 +1,7 @@
+#include "rctSigs.h"
+
+mgSig wasm_MLSAG_Gen(const key &message, const keyM &pk, const keyV &xx, const unsigned int index, size_t dsRows)
+    __attribute__((export_name("MLSAG_Gen")))
+{
+    return MLSAG_Gen(message, pk, xx, index, dsRows);
+}
