f0f6cda1a65e0280c31e94dc7327d2ac04562dae
Vasilii Rogin <vasya@rogin.ru>
2023-12-28 17:51:08 +0200

    Add more test for keccak hash

--- lib-mlsag-js/keccak.test.mjs
+++ lib-mlsag-js/keccak.test.mjs
@@ -6,7 +6,7 @@ import assert from "node:assert";
 test("Empty", () => {
   assert.strictEqual(
     array_to_hex(Keccak.keccak256(new Uint8Array([]))),
-    "c5d2460186f7233c927e7db2dcc703c0e500b653ca82273b7bfad8045d85a470",
+    "c5d2460186f7233c927e7db2dcc703c0e500b653ca82273b7bfad8045d85a470"
   );
 });
 
@@ -14,10 +14,12 @@ test("Sample string", () => {
   assert.strictEqual(
     array_to_hex(
       Keccak.keccak256(
-        hex_to_array("610d3d69deb94f51dae6e535b88482b5e7e6fab6a89913a8344b273a9f2c1b08"),
-      ),
+        hex_to_array(
+          "610d3d69deb94f51dae6e535b88482b5e7e6fab6a89913a8344b273a9f2c1b08"
+        )
+      )
     ),
-    "0b78ab4ef4ab57f34256d31bcf0d62b726c1319a1d789367173bba9b96975ca2",
+    "0b78ab4ef4ab57f34256d31bcf0d62b726c1319a1d789367173bba9b96975ca2"
   );
 });
 
@@ -33,10 +35,14 @@ const parts = [
   "1860c19616dd102fdbee8540beaa295ad13ed3bb731135dafc7e252fc75d9102",
   "29bc9edd1e22f0a2ab6a124d5f2aab43f3f959d2c736dcf852ef067278da3a01",
 ];
-const partsResult = "fd19cafbfbdd2ff63236c744c410796306c12e02083b58da99203c54ced55e71";
+const partsResult =
+  "fd19cafbfbdd2ff63236c744c410796306c12e02083b58da99203c54ced55e71";
 
 test("Long data", () => {
-  assert.strictEqual(array_to_hex(Keccak.keccak256(hex_to_array(parts.join("")))), partsResult);
+  assert.strictEqual(
+    array_to_hex(Keccak.keccak256(hex_to_array(parts.join("")))),
+    partsResult
+  );
 });
 
 test("Long data by parts", () => {
@@ -45,4 +51,12 @@ test("Long data by parts", () => {
     hash.update(hex_to_array(part));
   }
   assert.strictEqual(array_to_hex(hash.digest()), partsResult);
+});
+
+test("Test message", () => {
+  const h = Keccak.keccak256(Buffer.from("Test message"));
+  assert.strictEqual(
+    array_to_hex(h),
+    "ceb871db69754f583338155828aa4ed5d9c89918bd9227275b0a71515469254a"
+  );
 });
