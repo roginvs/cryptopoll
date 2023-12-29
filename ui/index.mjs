23dba799775c8d9e516e7ad014d1cae997310ed4
Vasilii Rogin <vasya@rogin.ru>
2023-12-29 03:26:57 +0200

    Add animations

--- ui/index.mjs
+++ ui/index.mjs
@@ -12,3 +12,35 @@ pages.addEventListener("scroll", (e) => {
 learn_more.addEventListener("click", () => {
   pages.scrollTop += pages.offsetHeight;
 });
+
+function addShowingAnimations() {
+  const observer = new IntersectionObserver(
+    (entries) => {
+      entries.forEach((entry) => {
+        const target = entry.target;
+        if (target instanceof HTMLElement) {
+          target.style.opacity = entry.isIntersecting ? "1" : "0";
+          target.style.transform = entry.isIntersecting
+            ? "translateX(0%)"
+            : "translateX(5%)";
+        }
+      });
+    },
+    {
+      //root: el,
+      rootMargin: "0px",
+      threshold: 1,
+    }
+  );
+  document
+    .querySelectorAll(".welcome_container p, .welcome_container h1")
+    .forEach((el) => {
+      observer.observe(el);
+      if (el instanceof HTMLElement) {
+        el.style.transition = "all 300ms ease-out";
+        el.style.transitionDelay =
+          el.tagName === "P" ? "600ms" : el.tagName === "H1" ? "300ms" : "";
+      }
+    });
+}
+addShowingAnimations();
