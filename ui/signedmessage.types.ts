06834b55e71860aac8fd862dcb326e0b69ba1630
Vasilii Rogin <vasya@rogin.ru>
2024-01-16 19:00:01 +0200

    Update type comment

--- ui/signedmessage.types.ts
+++ ui/signedmessage.types.ts
@@ -1,7 +1,12 @@
 import { LSAG_Signature } from "../lib-mlsag-js/ringct.types";
 
 export interface SignedMessage {
+  /** A message itself */
   m: string;
-  mh: string;
+  /** Message hash (keccak), to ensure that message was encoded correctly. Optional */
+  mh?: string;
+  /** Hash of public keys. Optional */
+  pkh?: string;
+  /** Signature */
   sig: LSAG_Signature<string>;
 }
