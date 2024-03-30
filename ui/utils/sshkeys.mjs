2a89fdd7454ebc48f9da303180015bd6f601573d
Vasilii Rogin <vasya@rogin.ru>
2024-03-30 16:55:04 +0200

    Add parser for openssh keys

---
+++ ui/utils/sshkeys.mjs
@@ -0,0 +1,61 @@
+const magicString = "ssh-ed25519";
+const base64magicWithLenPrefix = "AAAAC3NzaC1lZDI1NTE5";
+
+/** @param {string} s  */
+export function is_ssh_ed25519_public_key(s) {
+  return s.startsWith(magicString) || s.startsWith(base64magicWithLenPrefix);
+}
+
+/**
+ * @param {string} s
+ */
+function base64tobuf(s) {
+  // Contains symbols with codes 0x00 to 0xFF
+  const bufStr = atob(s);
+  const buf = new Uint8Array([...bufStr].map((c) => c.charCodeAt(0)));
+
+  return buf;
+}
+
+/**
+ * @param {string} s
+ */
+function get_public_key_buf_from_ssh_ed25519_public_key_base64(s) {
+  const buf = base64tobuf(s);
+  const prefixBuf = base64tobuf(base64magicWithLenPrefix);
+  if (buf.length !== prefixBuf.length + 4 + 0x20) {
+    throw new Error("Wrong length");
+  }
+
+  if (
+    buf[prefixBuf.length + 0] !== 0 ||
+    buf[prefixBuf.length + 1] !== 0 ||
+    buf[prefixBuf.length + 2] !== 0 ||
+    buf[prefixBuf.length + 3] !== 0x20
+  ) {
+    throw new Error("Wrong length part");
+  }
+
+  return buf.subarray(prefixBuf.length + 4);
+}
+
+/**
+ * @param {string} s
+ */
+export function get_public_key_buf_from_ssh_ed25519_public_key(s) {
+  if (!is_ssh_ed25519_public_key) {
+    throw new Error(`Not a ssh ed25519 public key`);
+  }
+  if (s.startsWith(magicString)) {
+    const base64 = s.split(/\s+/)[1];
+    if (!base64) {
+      throw new Error("Not a ssh ed25519 public key, no base64 part");
+    }
+    return get_public_key_buf_from_ssh_ed25519_public_key_base64(base64);
+  } else if (s.startsWith(base64magicWithLenPrefix)) {
+    const base64 = s.split(/\s+/)[0];
+    return get_public_key_buf_from_ssh_ed25519_public_key_base64(base64);
+  } else {
+    throw new Error("Not a ssh ed25519 public key");
+  }
+}
---
+++ ui/utils/sshkeys.test.mjs
@@ -0,0 +1,32 @@
+import assert from "assert";
+import { get_public_key_buf_from_ssh_ed25519_public_key } from "./sshkeys.mjs";
+import { describe, it } from "node:test";
+import { array_to_hex } from "../../lib-mlsag-js/bytes.mjs";
+
+describe("get_public_key_buf_from_ssh_ed25519_public_key", function () {
+  it("should return public key for valid SSH public key", function () {
+    const validKey =
+      "ssh-ed25519  AAAAC3NzaC1lZDI1NTE5AAAAIPmj4NfVFZBaGLXSuySp8z1Im4TyX/wZ/WoZBaaijR8v vasilii";
+
+    assert.strictEqual(
+      array_to_hex(get_public_key_buf_from_ssh_ed25519_public_key(validKey)),
+      "f9a3e0d7d515905a18b5d2bb24a9f33d489b84f25ffc19fd6a1905a6a28d1f2f"
+    );
+  });
+
+  it("should return public key for valid SSH public key with no prefix", function () {
+    const validKey =
+      "AAAAC3NzaC1lZDI1NTE5AAAAIPmj4NfVFZBaGLXSuySp8z1Im4TyX/wZ/WoZBaaijR8v vasilii";
+
+    assert.strictEqual(
+      array_to_hex(get_public_key_buf_from_ssh_ed25519_public_key(validKey)),
+      "f9a3e0d7d515905a18b5d2bb24a9f33d489b84f25ffc19fd6a1905a6a28d1f2f"
+    );
+  });
+
+  it(`should throw on non ed25519 ssh public key`, () => {
+    assert.throws(() =>
+      get_public_key_buf_from_ssh_ed25519_public_key("ssh-rsa asdasd")
+    );
+  });
+});
