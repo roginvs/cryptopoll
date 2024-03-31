0271d47e975d19d000576914bc482b7e9fb9510c
Vasilii Rogin <vasya@rogin.ru>
2024-03-31 19:48:46 +0300

    Add tests for asn1parse

--- ui/utils/asn1.test.mjs
+++ ui/utils/asn1.test.mjs
@@ -1,6 +1,7 @@
 import { describe, it } from "node:test";
-import { packIntForAsn } from "./asn1.mjs";
+import { asn1parse, packIntForAsn } from "./asn1.mjs";
 import assert from "node:assert";
+import { base64tobuf } from "./base64tobuf.mjs";
 
 describe(`Asn tools`, () => {
   describe(`packIntForAsn`, () => {
@@ -21,5 +22,33 @@ describe(`Asn tools`, () => {
           expected
         ));
     }
+  });
+
+  describe(`Asn1parse`, () => {
+    const [parsed1, rest1] = asn1parse(
+      base64tobuf(
+        "MC4CAQAwBQYDK2VwBCIEIBrpDOAV21AtzIp6HNaQo+HUFYZD96AV3ZFjQ61P1kTa"
+      )
+    );
+    assert.strictEqual(rest1.length, 0);
+
+    assert.deepStrictEqual(parsed1, {
+      type: "sequence",
+      value: [
+        { type: "integer", value: new Uint8Array([0]) },
+        {
+          type: "sequence",
+          value: [{ type: "oid", value: new Uint8Array([43, 101, 112]) }],
+        },
+        {
+          type: "octetstring",
+          value: new Uint8Array([
+            4, 32, 26, 233, 12, 224, 21, 219, 80, 45, 204, 138, 122, 28, 214,
+            144, 163, 225, 212, 21, 134, 67, 247, 160, 21, 221, 145, 99, 67,
+            173, 79, 214, 68, 218,
+          ]),
+        },
+      ],
+    });
   });
 });
